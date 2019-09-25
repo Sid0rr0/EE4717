d20f10821d2d917605ff7d3630e4f63da3a3bb38
lethanhh <lethanhh@fit.cvut.cz>
2019-09-25 19:30:23 +0800

    js for dynamic menu prices

---
+++ week06/CaseStudyPt3/js/menuPrices.js
@@ -0,0 +1,81 @@
+
+function updateItem1(e) {
+    const single = 2;
+    const price = parseFloat(e.target.value) * single;
+
+    document.getElementById("item1Price").innerText = "$" + price;
+    updateTotalPrice();
+}
+
+function updateItem2(e) {
+    const single = 2;
+    const double = 3;
+
+    let price = parseFloat(e.target.value);
+
+    if(isNaN(price)) {
+        if(document.getElementById("item2").value) {
+            if(document.getElementById("item2Radio1").checked)
+                price = parseFloat(document.getElementById("item2").value) * single;
+            else if (document.getElementById("item2Radio2").checked)
+                price = parseFloat(document.getElementById("item2").value) * double;
+        } else
+            price = 0
+    }
+    else if(document.getElementById("item2Radio1").checked)
+        price *= single;
+    else if (document.getElementById("item2Radio2").checked)
+        price *= double;
+
+    document.getElementById("item2Price").innerText = "$" + price;
+    updateTotalPrice();
+}
+
+function updateItem3(e) {
+    const single = 4.75;
+    const double = 5.75;
+
+    let price = parseFloat(e.target.value);
+
+    if(isNaN(price)) {
+        if(document.getElementById("item3").value) {
+            if(document.getElementById("item3Radio1").checked)
+                price = parseFloat(document.getElementById("item3").value) * single;
+            else if (document.getElementById("item3Radio2").checked)
+                price = parseFloat(document.getElementById("item3").value) * double;
+        } else
+            price = 0
+    }
+    else if(document.getElementById("item3Radio1").checked)
+        price *= single;
+    else if (document.getElementById("item3Radio2").checked)
+        price *= double;
+
+    document.getElementById("item3Price").innerText = "$" + price;
+
+    updateTotalPrice();
+}
+
+function updateTotalPrice() {
+
+    let price1 = document.getElementById("item1Price").innerText;
+    let price2 = document.getElementById("item2Price").innerText;
+    let price3 = document.getElementById("item3Price").innerText;
+
+    price1 = price1.substr(1);
+    price2 = price2.substr(1);
+    price3 = price3.substr(1);
+
+    let price = 0;
+
+    if(!isNaN(parseFloat(price1)))
+        price += parseFloat(price1);
+
+    if(!isNaN(parseFloat(price2)))
+        price += parseFloat(price2);
+
+    if(!isNaN(parseFloat(price3)))
+        price += parseFloat(price3);
+
+    document.getElementById("totalPrice").innerText = "$" + price;
+}
